246cd60806c48285078322faa964c1fa9840c2f2
Mitali <mitaligupta726@gmail.com>
2021-02-16 22:44:44 +0100

    GET req file upload

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require("fs");
+var multer  = require('multer');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,6 +11,7 @@ app.get('/index.html', (req, res) => {
     res.sendFile( __dirname + "/" + "index.html" );
 });
 
+// GET req of form
 app.get('/process_get', (req,res)=>{
     // Prepare output in JSON format
    response = {
@@ -20,7 +23,9 @@ app.get('/process_get', (req,res)=>{
 });
 // url encoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(multer({ dest: 'tmp/'}));
 
+// POST req for form
 app.post('/process_post',urlencodedParser, (req,res)=>{
     // Prepare output in JSON format
    response = {
@@ -30,6 +35,30 @@ app.post('/process_post',urlencodedParser, (req,res)=>{
     console.log(response)
     res.end(JSON.stringify(response));
 });
+
+// POST req of file upload
+app.post('file_upload', (req,res)=>{
+   console.log(req.files.file.name);
+   console.log(req.files.file.path);
+   console.log(req.files.file.type);
+   var file = __dirname + "/" + req.files.file.name;
+
+   fs.readFile( req.files.file.path, function (err, data) {
+    fs.writeFile(file, data, function (err) {
+       if( err ) {
+          console.log( err );
+          } else {
+             response = {
+                message:'File uploaded successfully',
+                filename:req.files.file.name
+             };
+          }
+       
+       console.log( response );
+       res.end( JSON.stringify( response ) );
+    });
+ });
+})
 var server = app.listen(3000, function(){
     var host = server.address().address
     var port = server.address().port
