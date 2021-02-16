d7a300b9aa44f40abd9a02468da3dcde95ee67b2
Mitali <mitaligupta726@gmail.com>
2021-02-16 23:48:05 +0100

    RESTAPI POSIX calls

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -4,6 +4,15 @@ var bodyParser = require('body-parser');
 var fs = require("fs");
 var multer  = require('multer');
 
+var user = {
+    "user4" : {
+       "name" : "jazzy",
+       "password" : "password4",
+       "profession" : "teacher",
+       "id": 4
+    }
+ }
+
 app.use(express.static(__dirname + '/public'));
 
 //GET homepage
@@ -23,7 +32,7 @@ app.get('/process_get', (req,res)=>{
 });
 // url encoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.use(multer({ dest: 'tmp/'}));
+//app.use(multer({ dest: 'tmp/'}));
 
 // POST req for form
 app.post('/process_post',urlencodedParser, (req,res)=>{
@@ -37,7 +46,7 @@ app.post('/process_post',urlencodedParser, (req,res)=>{
 });
 
 // POST req of file upload
-app.post('file_upload', (req,res)=>{
+/* app.post('file_upload', (req,res)=>{
    console.log(req.files.file.name);
    console.log(req.files.file.path);
    console.log(req.files.file.type);
@@ -58,7 +67,49 @@ app.post('file_upload', (req,res)=>{
        res.end( JSON.stringify( response ) );
     });
  });
-})
+}) */
+
+// GET json req
+app.get('/listUsers', (req,res)=> {
+    fs.readFile(__dirname + '/public' +'/' + "users.json", 'utf8', (err,data)=> {
+        console.log(data);
+        res.end( data );
+    });
+});
+// POST json req
+app.post('/addUser', (req,res)=> { 
+    fs.readFile(__dirname + '/public' +'/' + "users.json", 'utf8', (err,data)=> {
+        data = JSON.parse( data );
+        data["user4"] = user["user4"];
+        console.log(data);
+        res.end( JSON.stringify(data));
+    });
+});
+
+//show details
+
+app.get('/:id', (req,res)=> {
+    fs.readFile(__dirname + '/public' +'/' + "users.json", 'utf8', (err,data)=>{ 
+        users = JSON.parse(data);
+        user = users["user" + req.params.id]
+        console.log(user);
+        res.end( JSON.stringify(user));
+
+    });
+});
+
+//delete user
+
+app.delete('/deleteUser', (req, res)=> {
+    // First read existing users.
+    fs.readFile(__dirname + '/public' +'/' + "users.json", 'utf8', (err,data)=> {
+        data = JSON.parse( data );
+       delete data["user" + 2];
+        
+       console.log( data );
+       res.end( JSON.stringify(data));
+    });
+ })
 var server = app.listen(3000, function(){
     var host = server.address().address
     var port = server.address().port
